ba9fcc4f598eedc033623a7887723c3019d37249
totohub <trada006@gmail.com>
2020-10-28 20:23:29 +1030

    test git status git status git commit -m test

--- Observer Design Pattern/src/Demo.java
+++ Observer Design Pattern/src/Demo.java
@@ -4,9 +4,9 @@ public class Demo {
 	public static void main(String[] args) {
 		MyModel model = new MyModel();
 		MyObserver observer = new MyObserver(model);
-		model.getPersons().get(0).setLastName("Tran");
-		model.getPersons().get(1).setFirstName("Porn");
-		
+		model.getPersons().get(0).setLastName("wrong");
+		model.getPersons().get(1).setFirstName("wrong");
+		//this is wrong
 
 	}
 
