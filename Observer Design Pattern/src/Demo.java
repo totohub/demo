600cde88b17a092fa5d13c597159e26bc5c8a4c7
totohub <trada006@gmail.com>
2020-10-28 20:30:17 +1030

    lol

--- Observer Design Pattern/src/Demo.java
+++ Observer Design Pattern/src/Demo.java
@@ -4,9 +4,9 @@ public class Demo {
 	public static void main(String[] args) {
 		MyModel model = new MyModel();
 		MyObserver observer = new MyObserver(model);
-		model.getPersons().get(0).setLastName("wrong");
-		model.getPersons().get(1).setFirstName("wrong");
-		//this is wrong
+		model.getPersons().get(0).setLastName("lol");
+		model.getPersons().get(1).setFirstName("lol");
+		//lol
 
 	}
 
